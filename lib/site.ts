079dade014822f0a0b91c87aeed23e028a13fa30
Angie Lee <angielxx94@gmail.com>
2026-06-19 13:58:51 +0900

    chore: 사이트 메타데이터 업데이트
    SITE_NAME, AUTHOR_NAME, SITE_DESCRIPTION 변경

--- lib/site.ts
+++ lib/site.ts
@@ -1,6 +1,6 @@
 export const SITE_URL = "https://angielee-dev.vercel.app";
-export const SITE_NAME = "More than Better";
-export const SITE_DESCRIPTION = "프론트엔드 개발자 angielee의 블로그 겸 포트폴리오";
-export const AUTHOR_NAME = "angielee";
+export const SITE_NAME = "Angie Lee";
+export const SITE_DESCRIPTION = "Still exploring, still building. 프론트엔드 개발과 성장의 기록을 담은 공간입니다.";
+export const AUTHOR_NAME = "이은지";
 export const AUTHOR_EMAIL = "angielxx94@gmail.com";
 export const AUTHOR_GITHUB = "https://github.com/angielxx";
